86f3cdc887b1c95526f918b812c8c8c8955e7ef3
Commandcracker <lukas.Fricke.dev@gmail.com>
2023-01-27 16:42:20 +0100

    better Pet Simulator X

--- games/2316994223.lua
+++ games/2316994223.lua
@@ -5,15 +5,48 @@ local Players = game:GetService("Players")
 
 local LocalPlayer = Players.LocalPlayer
 
--- Auto Collect
-local AutoCollect = Page:addToggle("Auto Collect")
+-- Auto Collect Orbs
+local AutoCollectOrbs = Page:addToggle("Auto Collect Orbs")
 
 Threads:Add(function()
     while wait() do
-        if AutoCollect:IsEnabeld() then
-            for _, v in pairs(workspace["__THINGS"].Orbs:children()) do
+        if AutoCollectOrbs:IsEnabeld() then
+            for _, v in pairs(workspace["__THINGS"].Orbs:GetChildren()) do
                 v.CFrame = LocalPlayer.Character.HumanoidRootPart.CFrame
             end
+        end
+    end
+end)
+
+-- Auto Collect Lootbags
+local AutoCollectLootbags = Page:addToggle("Auto Collect Lootbags")
+
+Threads:Add(function()
+    while wait() do
+        if AutoCollectLootbags:IsEnabeld() then
+            for _, v in pairs(workspace["__THINGS"].Lootbags:GetChildren()) do
+                v.CFrame = LocalPlayer.Character.HumanoidRootPart.CFrame
+            end
+        end
+    end
+end)
+
+-- Auto Collect Gifts
+local AutoCollectGifts = Page:addToggle("Auto Collect Gifts")
+
+Threads:Add(function()
+    while wait() do
+        if AutoCollectGifts:IsEnabeld() then
+            for _, v in pairs(ReplicatedStorage:GetChildren()) do
+                if v.Name == "RemoteFunction" then
+                    for i = 1, 12 do
+                        pcall(function()
+                            v:InvokeServer(i)
+                        end)
+                    end
+                end
+            end
+            wait(1)
         end
     end
 end)
@@ -26,7 +59,7 @@ local function FindNearestCoin()
     local found
     local last = math.huge
 
-    for _, Coin in pairs(workspace["__THINGS"].Coins:children()) do
+    for _, Coin in pairs(workspace["__THINGS"].Coins:GetChildren()) do
         local distance = LocalPlayer:DistanceFromCharacter(Coin.POS.Position)
         if distance < last then
             found = Coin
@@ -40,19 +73,14 @@ Threads:Add(function()
     while wait() do
         if AutoFarm:IsEnabeld() then
             local Coin = FindNearestCoin()
-            local last_count
-            repeat
-                last_count = #Coin.Pets:GetChildren()
+            local pets = tonumber(LocalPlayer.PlayerGui.Main.Bottom.Inventory.Equipped.Text:split("/")[1])
+
+            for _ = 1, pets do
                 Framework.Signal.Fire("Select Coin", Coin)
                 wait(0.1)
-            until (#Coin:GetChildren() == 0 or last_count > #Coin.Pets:GetChildren())
-            for _ = 1, last_count do
-                if #Coin:GetChildren() ~= 0 then
-                    Framework.Signal.Fire("Select Coin", Coin)
-                    wait(0.1)
-                end
             end
-            while #Coin:GetChildren() ~= 0 do
+
+            while #Coin:GetChildren() ~= 0 and AutoFarm:IsEnabeld() do
                 wait()
             end
         end
